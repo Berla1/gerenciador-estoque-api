63646c7f519dcf397d40af10592b575949962381
Gustavo Berlanga <74972093+Berla1@users.noreply.github.com>
2025-03-12 18:02:53 -0300

    feat: metodo put

--- src/main/java/com/br/gerenciamento_estoque/controller/ProdutoController.java
+++ src/main/java/com/br/gerenciamento_estoque/controller/ProdutoController.java
@@ -4,9 +4,11 @@ import com.br.gerenciamento_estoque.dto.ProdutoDTO;
 import com.br.gerenciamento_estoque.model.Produto;
 import com.br.gerenciamento_estoque.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/produtos")
@@ -30,4 +32,25 @@ public class ProdutoController {
     public void deleteProduto(@PathVariable Long id){
         produtoRepository.deleteById(id);
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Produto> atualizarProduto(@PathVariable Long id, @RequestBody ProdutoDTO produtoDTO) {
+        Produto produto = produtoRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Produto não encontrado com ID: " + id));
+
+        produto.setNome(produtoDTO.nome());
+        produto.setPreco(produtoDTO.preco());
+        produto.setQuantidade_estoque(produtoDTO.quantidade_estoque());
+        produto.setFabricante(produtoDTO.fabricante());
+        produto.setFornecedor(produtoDTO.fornecedor());
+        produto.setCodigo_barras(produtoDTO.codigo_barras());
+
+        produtoRepository.save(produto);
+
+        return ResponseEntity.ok(produto);
+    }
+
+
+
+
 }
