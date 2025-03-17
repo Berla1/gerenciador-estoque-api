aeb29fa67e6bd8b168c6007dc608363aaff29b86
Gustavo Berlanga <74972093+Berla1@users.noreply.github.com>
2025-03-17 16:28:05 -0300

    feat: criando model de usuario, e começando auth

---
+++ src/main/java/com/br/gerenciamento_estoque/model/Usuario.java
@@ -0,0 +1,82 @@
+package com.br.gerenciamento_estoque.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+public class Usuario implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String login;
+    private String senha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Usuario setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public Usuario setLogin(String login) {
+        this.login = login;
+        return this;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public Usuario setSenha(String senha) {
+        this.senha = senha;
+        return this;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
